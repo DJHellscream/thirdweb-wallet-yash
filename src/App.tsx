90ccb009fea446346d386cd22712746eb379fa85
Trevor Collins <25141495+DJHellscream@users.noreply.github.com>
2024-11-21 13:40:51 -0500

    added client.ts settings and connect button settings.

--- src/App.tsx
+++ src/App.tsx
@@ -1,98 +1,104 @@
 import { ConnectButton } from "thirdweb/react";
 import thirdwebIcon from "./thirdweb.svg";
-import { client } from "./client";
+import { chains, client, wallets } from "./client";
 
 export function App() {
-	return (
-		<main className="p-4 pb-10 min-h-[100vh] flex items-center justify-center container max-w-screen-lg mx-auto">
-			<div className="py-20">
-				<Header />
+  return (
+    <main className="p-4 pb-10 min-h-[100vh] flex items-center justify-center container max-w-screen-lg mx-auto">
+      <div className="py-20">
+        <Header />
 
-				<div className="flex justify-center mb-20">
-					<ConnectButton
-						client={client}
-						appMetadata={{
-							name: "Example app",
-							url: "https://example.com",
-						}}
-					/>
-				</div>
+        <div className="flex justify-center mb-20">
+          <ConnectButton
+            client={client}
+            appMetadata={{
+              name: "Example app",
+              url: "https://example.com",
+            }}
+            autoConnect={true}
+            chains={chains}
+            wallets={wallets}
+            detailsModal={{
+              assetTabs: ["token", "nft"],
+            }}
+          />
+        </div>
 
-				<ThirdwebResources />
-			</div>
-		</main>
-	);
+        <ThirdwebResources />
+      </div>
+    </main>
+  );
 }
 
 function Header() {
-	return (
-		<header className="flex flex-col items-center mb-20 md:mb-20">
-			<img
-				src={thirdwebIcon}
-				alt=""
-				className="size-[150px] md:size-[150px]"
-				style={{
-					filter: "drop-shadow(0px 0px 24px #a726a9a8)",
-				}}
-			/>
+  return (
+    <header className="flex flex-col items-center mb-20 md:mb-20">
+      <img
+        src={thirdwebIcon}
+        alt=""
+        className="size-[150px] md:size-[150px]"
+        style={{
+          filter: "drop-shadow(0px 0px 24px #a726a9a8)",
+        }}
+      />
 
-			<h1 className="text-2xl md:text-6xl font-bold tracking-tighter mb-6 text-zinc-100">
-				thirdweb SDK
-				<span className="text-zinc-300 inline-block mx-1"> + </span>
-				<span className="inline-block -skew-x-6 text-violet-500"> vite </span>
-			</h1>
+      <h1 className="mb-6 text-2xl font-bold tracking-tighter md:text-6xl text-zinc-100">
+        thirdweb SDK
+        <span className="inline-block mx-1 text-zinc-300"> + </span>
+        <span className="inline-block -skew-x-6 text-violet-500"> vite </span>
+      </h1>
 
-			<p className="text-zinc-300 text-base">
-				Read the{" "}
-				<code className="bg-zinc-800 text-zinc-300 px-2 rounded py-1 text-sm mx-1">
-					README.md
-				</code>{" "}
-				file to get started.
-			</p>
-		</header>
-	);
+      <p className="text-base text-zinc-300">
+        Read the{" "}
+        <code className="px-2 py-1 mx-1 text-sm rounded bg-zinc-800 text-zinc-300">
+          README.md
+        </code>{" "}
+        file to get started.
+      </p>
+    </header>
+  );
 }
 
 function ThirdwebResources() {
-	return (
-		<div className="grid gap-4 lg:grid-cols-3 justify-center">
-			<ArticleCard
-				title="thirdweb SDK Docs"
-				href="https://portal.thirdweb.com/typescript/v5"
-				description="thirdweb TypeScript SDK documentation"
-			/>
+  return (
+    <div className="grid justify-center gap-4 lg:grid-cols-3">
+      <ArticleCard
+        title="thirdweb SDK Docs"
+        href="https://portal.thirdweb.com/typescript/v5"
+        description="thirdweb TypeScript SDK documentation"
+      />
 
-			<ArticleCard
-				title="Components and Hooks"
-				href="https://portal.thirdweb.com/typescript/v5/react"
-				description="Learn about the thirdweb React components and hooks in thirdweb SDK"
-			/>
+      <ArticleCard
+        title="Components and Hooks"
+        href="https://portal.thirdweb.com/typescript/v5/react"
+        description="Learn about the thirdweb React components and hooks in thirdweb SDK"
+      />
 
-			<ArticleCard
-				title="thirdweb Dashboard"
-				href="https://thirdweb.com/dashboard"
-				description="Deploy, configure, and manage your smart contracts from the dashboard."
-			/>
-		</div>
-	);
+      <ArticleCard
+        title="thirdweb Dashboard"
+        href="https://thirdweb.com/dashboard"
+        description="Deploy, configure, and manage your smart contracts from the dashboard."
+      />
+    </div>
+  );
 }
 
 function ArticleCard(props: {
-	title: string;
-	href: string;
-	description: string;
+  title: string;
+  href: string;
+  description: string;
 }) {
-	return (
-		<a
-			href={`${props.href}?utm_source=vite-template`}
-			target="_blank"
-			className="flex flex-col border border-zinc-800 p-4 rounded-lg hover:bg-zinc-900 transition-colors hover:border-zinc-700"
-			rel="noreferrer"
-		>
-			<article>
-				<h2 className="text-lg font-semibold mb-2">{props.title}</h2>
-				<p className="text-sm text-zinc-400">{props.description}</p>
-			</article>
-		</a>
-	);
+  return (
+    <a
+      href={`${props.href}?utm_source=vite-template`}
+      target="_blank"
+      className="flex flex-col p-4 transition-colors border rounded-lg border-zinc-800 hover:bg-zinc-900 hover:border-zinc-700"
+      rel="noreferrer"
+    >
+      <article>
+        <h2 className="mb-2 text-lg font-semibold">{props.title}</h2>
+        <p className="text-sm text-zinc-400">{props.description}</p>
+      </article>
+    </a>
+  );
 }
--- src/client.ts
+++ src/client.ts
@@ -1,9 +1,83 @@
+import { defineChain as viemDefineChain, http } from "viem";
 import { createThirdwebClient } from "thirdweb";
+import {
+  base as BaseChain,
+  avalanche as AvaxChain,
+  mainnet as EthChain,
+  avalancheFuji as FujiChain,
+  defineChain,
+} from "thirdweb/chains";
+import { createWallet, inAppWallet, Wallet } from "thirdweb/wallets";
 
-// Replace this with your client ID string
-// refer to https://portal.thirdweb.com/typescript/v5/client on how to get a client ID
-const clientId = import.meta.env.VITE_TEMPLATE_CLIENT_ID;
+const thirdWebClientId = import.meta.env.VITE_THIRDWEB_CLIENT_ID;
+
+function isMobileDevice() {
+  // Check if the user agent matches common mobile devices
+  const userAgentCheck =
+    /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+      navigator.userAgent
+    );
+
+  // Check if touch is supported
+  const touchCheck = "ontouchstart" in window || navigator.maxTouchPoints > 0;
+
+  return userAgentCheck || touchCheck;
+}
+
+// Example usage
+if (isMobileDevice()) {
+  console.log("This is a mobile device.");
+  // Use the mobile version of app.core
+} else {
+  console.log("This is a PC.");
+  // Use the desktop version of app.core
+}
+
+// ======================= THIRDWEB =======================
 
 export const client = createThirdwebClient({
-  clientId: clientId,
+  clientId: thirdWebClientId,
 });
+
+export const chains = [AvaxChain, BaseChain, FujiChain];
+
+const inAppWallets: Wallet<"inApp"> = inAppWallet({
+  auth: {
+    options: [
+      "x",
+      "apple",
+      "google",
+      "discord",
+      "telegram",
+      "github",
+      "steam",
+      "twitch",
+      "coinbase",
+      "farcaster",
+      "line",
+      "email",
+      "facebook",
+      "passkey",
+      "phone",
+    ],
+    mode: undefined,
+  },
+  hidePrivateKeyExport: false,
+});
+
+export const wallets = [
+  inAppWallets,
+  ...(isMobileDevice()
+    ? [createWallet("app.core")]
+    : [createWallet("app.core.extension")]),
+  createWallet("io.rabby"),
+  createWallet("app.phantom"),
+  createWallet("io.metamask"),
+  createWallet("com.coinbase.wallet"),
+  createWallet("walletConnect"),
+  createWallet("me.rainbow"),
+  createWallet("io.zerion.wallet"),
+  createWallet("com.trustwallet.app"),
+  createWallet("com.ledger"),
+  createWallet("com.crypto.wallet"),
+];
